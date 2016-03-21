dc1be41a33368b87714c35b74f5369fe4629faf4
Demon Zhang <demon.zhang@samesamechina.com>
2016-03-21 14:52:34 +0800

    no message

--- index.php
+++ index.php
@@ -1,28 +1,6 @@
 <?php
-ini_set("display_errors", 1);
-Session_Start();
-require_once dirname(__FILE__) . "/conf/config.php";
-require_once dirname(__FILE__) . "/module/module.php";
-require_once dirname(__FILE__) . "/module/emoji.php";
 
+require_once dirname(__FILE__) . "/core/bootstrap.php";
 
-$routers = array();
-$routers['/api/getdata'] = '_wechat_getdata';
-$routers['/api/callback'] = '_wechat_callback';
-$routers['/api/saveimg'] = '_api_saveimg';
-$routers['/home'] = '_page_home';
-$routers['/upfile'] = '_page_upfile';
-$routers['/share'] = '_page_share';
+Core::Response();
 
-$listener = array();
-$listener[] = '/';
-$listener[] = '/index.php';
-
-$current_router = preg_replace('/\?.*/', '', $_SERVER['REQUEST_URI']);
-
-if(in_array($current_router, $listener))
- 	call_user_func_array('_access_listener', array());
-
-if(isset($routers[$current_router]))
-	call_user_func_array($routers[$current_router], array());
-?>
--- lib/UserAPI.php
+++ lib/UserAPI.php
@@ -1,33 +1,87 @@
 <?php
+namespace Lib;
 
-class UserAPI {
+class UserAPI extends Base {
 
-	private $_db;
+  public function retrieveGroups() {
+    $url = "https://api.weixin.qq.com/cgi-bin/groups/get?access_token={$this->_access_token}";
+    $return = file_get_contents($url);
+    return json_decode($return);
+  }
 
-	public function __construct() {
-		$this->_db = new DatabaseAPI();
-	}
+  public function createGroup(array $data) {
+    $url = "https://api.weixin.qq.com/cgi-bin/groups/create?access_token={$this->_access_token}";
+    $json = json_encode($data, JSON_UNESCAPED_UNICODE);
+    // post data to wechat
+    $return = $this->postData($url, $json);
+    return $return;
+  }
 
-	public function userLoad($type = false){
-		if(isset($_SESSION['user'])){
-			return $_SESSION['user'];
-		} else {
-			if ($type == true) {
-				return false;
-			}
-			$WechatAPI = new WechatAPI();
-			$WechatAPI->wechatAuthorize();
-		}
-		
-	}
+  public function updateGroupName(array $data) {
+    $url = "https://api.weixin.qq.com/cgi-bin/groups/update?access_token={$this->_access_token}";
+    $json = json_encode($data, JSON_UNESCAPED_UNICODE);
+    // post data to wechat
+    $return = $this->postData($url, $json);
+    return $return;
+  }
 
-	public function userLogin($openid){
-		$result = $this->_db->findUserByOpenid($openid);
-		$user = $result ? $result : $this->userRegister($openid);
-		return $user;
-	}
+  public function deleteGroup(array $data) {
+    $url = "https://api.weixin.qq.com/cgi-bin/groups/delete?access_token={$this->_access_token}";
+    $json = json_encode($data, JSON_UNESCAPED_UNICODE);
+    // post data to wechat
+    $return = $this->postData($url, $json);
+    return $return;
+  }
 
-	public function userRegister($openid){
-		return $this->_db->insertUser($openid);
-	}
+  public function moveMemberGroup(array $data) {
+    $url = "https://api.weixin.qq.com/cgi-bin/groups/members/update?access_token={$this->_access_token}";
+    $json = json_encode($data, JSON_UNESCAPED_UNICODE);
+    // post data to wechat
+    $return = $this->postData($url, $json);
+    return $return;
+  }
+
+  public function moveMemberGroupBatch(array $data) {
+    $url = "https://api.weixin.qq.com/cgi-bin/groups/members/batchupdate?access_token={$this->_access_token}";
+    $json = json_encode($data, JSON_UNESCAPED_UNICODE);
+    // post data to wechat
+    $return = $this->postData($url, $json);
+    return $return;
+  }
+
+  public function getUserInfo($openid) {
+    $url = "https://api.weixin.qq.com/cgi-bin/user/info?access_token={$this->_access_token}&openid={$openid}&lang=zh_CN";
+    $return = file_get_contents($url);
+    return json_decode($return);
+  }
+
+  public function isUserSubscribed($openid, $access_token) {
+    $url = "https://api.weixin.qq.com/cgi-bin/user/info?access_token={$this->_access_token}&openid={$openid}&lang=zh_CN";
+    $return = file_get_contents($url);
+    $rs = json_decode($return);
+    if(isset($rs->subscribe) && $rs->subscribe == 1)
+      return TRUE;
+    else
+      return FALSE;
+  }
+
+  public function getAuthorizeUrl($appid, $callback, $scope) {
+    $redirect_uri = urlencode($callback);
+    $url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid={$appid}&redirect_uri={$redirect_uri}&response_type=code&scope={$scope}&state=STATE#wechat_redirect";
+    return $url;
+  }
+
+  public function getSnsAccessToken($code, $appid, $appsecret) {
+    $url = "https://api.weixin.qq.com/sns/oauth2/access_token?code={$code}&grant_type=authorization_code&appid={$appid}&secret={$appsecret}";
+    $return = file_get_contents($url);
+    return json_decode($return);
+  }
+
+  public function getSnsUserInfo($openid, $sns_access_token) {
+      $url = "https://api.weixin.qq.com/sns/userinfo?access_token={$sns_access_token}&openid={$openid}&lang=zh_CN";
+      $userinfo = file_get_contents($url);
+      $userinfo = json_decode($userinfo);
+      return $userinfo;
+  }
+
 }
