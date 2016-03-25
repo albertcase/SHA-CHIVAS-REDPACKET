f268907e2c3bc90ba8d8e5b3117c75cb68947553
Demon Zhang <demon.zhang@samesamechina.com>
2016-03-25 18:19:59 +0800

    no message

--- bundle/ChivasBundle/ApiController.php
+++ bundle/ChivasBundle/ApiController.php
@@ -19,8 +19,8 @@ class ApiController extends Controller {
 			return $this->statusPrint(0, '未登录');
 		}
 
-		if (isset($_SESSION['msg_code'])) {
-			return $this->statusPrint(3, '未登录');
+		if (isset($_SESSION['msg_code']) && $_SESSION['msg_code'] - time() <= 60) {
+			return $this->statusPrint(3, '短信已经发出');
 		}
 		$request = $this->Request();
 		$fields = array(
@@ -30,20 +30,29 @@ class ApiController extends Controller {
 		$mobile = $request->request->get('mobile');
 		$sms = new \Lib\SmsAPI();
 		$code = $sms->sendMessage($user->uid, $mobile);
-
+		$_SESSION['msg_code'] = time();
 		return $this->statusPrint(1, '提交成功');
 	}
 
 
 	public function submitAction() {
+		$UserAPI = new \Lib\UserAPI();
+		$user = $UserAPI->userLoad(true);
+		if (!$user) {
+			return $this->statusPrint(0, '未登录');
+		}
+
+		if (!isset($_SESSION['msg_code'])) {
+			return $this->statusPrint(3, '请先请求验证码');
+		}
 		$request = $this->Request();
 		$fields = array(
 			'mobile' => array('mobile', '3'),
 			'code' => array('notnull', '3'),
 		);
 		$request->validation($fields);
-		$mobile = $request->query->get('mobile');
-		$code = $request->query->get('code');
+		$mobile = $request->request->get('mobile');
+		$code = $request->request->get('code');
 		return $this->statusPrint(1, '提交成功');
 		
 	}
