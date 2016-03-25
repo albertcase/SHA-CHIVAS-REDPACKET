e50bf598dcf95469e4cee30e9955284902d2c73c
Demon Zhang <demon.zhang@samesamechina.com>
2016-03-25 18:21:14 +0800

    no message

--- config/router.php
+++ config/router.php
@@ -9,5 +9,6 @@ $routers['/curio/app'] = array('ChivasBundle\Curio', 'index');
 $routers['/curio/oauth'] = array('ChivasBundle\Curio', 'oauth');
 $routers['/curio/regoauth'] = array('ChivasBundle\Curio', 'regoauth');
 $routers['/curio/regjs'] = array('ChivasBundle\Curio', 'regjs');
+$routers['/curio/regqrcode'] = array('ChivasBundle\Curio', 'regqrcode');
 $routers['/curio/callback'] = array('ChivasBundle\Curio', 'callback');
 $routers['/curio/redirect'] = array('ChivasBundle\Curio', 'redirect');
