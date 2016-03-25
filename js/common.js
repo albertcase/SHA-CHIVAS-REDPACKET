0da54374a1bc09d9152de6f71c61883494056eab
evelynzhang88 <1054982653@qq.com>
2016-03-25 16:33:11 +0800

    api share

--- js/common.js
+++ js/common.js
@@ -1,36 +1,3 @@
-//just for test
-//var CANSHAKE = '1';
-//var lotteryList = {
-//	code:'1',
-//	msg:[1,2,3]
-//}
-
-function addCard(i) {
-	wx.addCard({
-		cardList: [{
-			cardId: cardListJSON[i-1].cardId,
-			cardExt: '{"timestamp":"'+cardListJSON[i-1].cardExt.timestamp+'","signature":"'+cardListJSON[i-1].cardExt.signature+'"}'
-		}],
-		success: function(res) {
-			var cardList = res.cardList;
-			//alert(JSON.stringfiy(res));
-		},
-		fail: function(res) {
-			//alert(JSON.stringfiy(res));
-		},
-		complete: function(res) {
-			//alert(JSON.stringfiy(res));
-		},
-		cancel: function(res) {
-			//alert(JSON.stringfiy(res));
-		},
-		trigger: function(res) {
-			//alert(JSON.stringfiy(res));
-		}
-	});
-
-};
-
 function gotoPin(i) {
 	var $pin = $('.wrapper .pin');
 	$pin.removeClass('current').eq(i).addClass('current');
@@ -59,8 +26,6 @@ function gotoPin(i) {
 
 
 	};
-
-	//alert(Common.isWx());
 
 	this.Common = Common;
 
