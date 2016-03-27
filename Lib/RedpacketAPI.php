1a75fbeee8e1f7e8cc931492d82d865594e1baf1
Demon Zhang <demon.zhang@samesamechina.com>
2016-03-27 12:19:24 +0800

    no message

--- Lib/RedpacketAPI.php
+++ Lib/RedpacketAPI.php
@@ -20,7 +20,7 @@ class RedpacketAPI extends Base {
             'mch_id' => WX_BUSID,
             'wxappid' => WX_APPID,
             'send_name' => '芝华士Chivas',
-            're_openid' => $openid,
+            're_openid' => $user->openid,
             //'re_openid' => 'o3vWouHPZ73bIf5jyIZ9xea9fEfg',
             //'re_openid' => 'o3vWouBrq-b73OV25cFXhZNboy_k',
             
@@ -77,35 +77,6 @@ class RedpacketAPI extends Base {
         return strtoupper(md5($str)); 
     }
 
-    private function curl_post_ssl($url, $vars, $second=30,$aHeader=array())
-    {
-        $ch = curl_init();
-        curl_setopt($ch,CURLOPT_TIMEOUT,$second);
-        curl_setopt($ch,CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch,CURLOPT_URL,$url);
-        curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,false);
-        curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,false);    
-        curl_setopt($ch,CURLOPT_SSLCERT,$_SERVER['DOCUMENT_ROOT'].'../../cert/apiclient_cert.pem');
-        curl_setopt($ch,CURLOPT_SSLKEY,$_SERVER['DOCUMENT_ROOT'].'../../cert/apiclient_key.pem');      
-     
-        if( count($aHeader) >= 1 ){
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $aHeader);
-        }
-     
-        curl_setopt($ch,CURLOPT_POST, 1);
-        curl_setopt($ch,CURLOPT_POSTFIELDS,$vars);
-        $data = curl_exec($ch);
-        if($data){
-            curl_close($ch);
-            return $data;
-        }
-        else { 
-            $error = curl_errno($ch);
-            echo "call faild, errorCode:$error\n"; 
-            curl_close($ch);
-            return false;
-        }
-    }
 
 
 
